830da75ee811190ca967871deb45fa4ac72f7bec
Harsh Mishra <harsh.mishra@dream11.com>
2026-01-08 17:42:04 +0530

    fix: receive apikey from panel

--- src/App.tsx
+++ src/App.tsx
@@ -5,9 +5,11 @@ import AudienceDetails from './pages/audiencedetails'
 import Connections from './pages/connections'
 import ComponentsShowcase from './pages/components'
 import { useRouteSync } from './hooks/route'
+import { useParentCommunication } from './hooks/useParentCommunication'
 
 function App() {
   useRouteSync();
+  useParentCommunication();
   return (
     <div style={{ height: '100vh', width: '100vw', overflow: 'auto' }}>
       <Routes>

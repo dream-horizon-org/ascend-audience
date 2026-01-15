5e181701ca4f62d2b97975d41d94cd8d97ddbf03
Harsh Mishra <harsh.mishra@dream11.com>
2026-01-15 14:00:20 +0530

    fix: remove irrelevant items from table

--- vite.config.ts
+++ vite.config.ts
@@ -4,8 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    port: 9001,
-    strictPort: true, // Exit if port is already in use
-  }
+  publicDir: 'src/public', // Use src/public as the public directory
 })
